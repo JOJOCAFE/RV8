b2cde335b2a459ecbfccdf45f63409a8ce08fbcb
JoTaijiquan <taijimaster@gmail.com>
2026-05-27 13:32:43 +0700

    initial commit

---
+++ RV8GR/programs/test_isa.asm
@@ -0,0 +1,47 @@
+; RV8-GR Full ISA Test
+; Assembles to ROM at $8000
+; Expected: halts at 'pass' label with AC=$00, Z=1
+
+start:
+    LI $10          ; AC=$10
+    ADDI $05        ; AC=$15
+    SUBI $15        ; AC=$00, Z=1
+    XORI $AA        ; AC=$AA, Z=0
+    MV $00, a0      ; RAM[0]=$AA
+    MV $01, a0      ; RAM[1]=$AA
+    LI $00          ; AC=$00
+    MV a0, $00      ; AC=RAM[0]=$AA
+    SUB $01         ; AC=$AA-$AA=$00, Z=1
+    BEQ ok1
+    HLT             ; FAIL
+
+ok1:
+    LI $FF          ; AC=$FF, Z=0
+    BEQ fail1       ; should NOT take (Z=0)
+    BNE ok2         ; should take (Z=0)
+fail1:
+    HLT             ; FAIL
+
+ok2:
+    MV $02, a0      ; RAM[2]=$FF
+    LI $55          ; AC=$55
+    ADD $02         ; AC=$55+$FF=$54
+    XOR $02         ; AC=$54^$FF=$AB
+    SETPG $90       ; PG=$90
+    J lo(page90)    ; jump to $9000
+
+.ORG $9000
+page90:
+    SETPG $80       ; PG=$80
+    J lo(back)      ; jump back to $802E
+
+.ORG $802E
+back:
+    LI $80          ; AC=$80
+    MV $03, a0      ; RAM[3]=$80
+    SETPG_R $03     ; PG=RAM[3]=$80
+    LI $00          ; AC=$00
+    SUBI $00        ; AC=$00, Z=1
+
+pass:
+    HLT             ; ALL PASS
